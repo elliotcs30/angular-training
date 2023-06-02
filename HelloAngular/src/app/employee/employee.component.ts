13cb4bd176fc9b10fc8810344b385518bf6a605e
Elliot <elliotcs30@gmail.com>
2023-06-02 23:02:48 +0800

    feat: add binding event

--- HelloAngular/src/app/employee/employee.component.ts
+++ HelloAngular/src/app/employee/employee.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent {
+  name = 'Angular'
 
+  purchaseCount = 10;
+  unitPrice = 200;
+
+  fontSize = 12;
+
+  onSetFontSize(value: number): void {
+    this.fontSize += value;
+  }
 }
