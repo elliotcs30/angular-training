5baab23815c84bf4c3dacc76900a27f24db56cf1
elliot <elliotcs30@gmail.com>
2023-06-04 10:20:49 +0800

    feat: add ng-content

--- HelloAngular/src/app/app.module.ts
+++ HelloAngular/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { EmployeeComponent } from './employee/employee.component';
 import { FontSizeComponent } from './font-size/font-size.component';
 import { FontSize2Component } from './font-size2/font-size2.component';
 import { FontSize3Component } from './font-size3/font-size3.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FontSize3Component } from './font-size3/font-size3.component';
     EmployeeComponent,
     FontSizeComponent,
     FontSize2Component,
-    FontSize3Component
+    FontSize3Component,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
